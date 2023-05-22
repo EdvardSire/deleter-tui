daf58d0fca2cd7e002ce590e6a77f56e8405862f
EdvardSire <edvardsire@protonmail.com>
2023-05-22 18:37:02 +0200

    fix messy backward navigation

--- draw.c
+++ draw.c
@@ -10,15 +10,12 @@
 
 //
 void print_menu(WINDOW *menu_win, int highlight);
-void get_dir_info();
 void change_dir();
 void dir_back(WINDOW *menu_win);
 ///
-#define MAX_FILE_NUMBER 10
+#define MAX_FILE_NUMBER 100
 #define MAX_FILE_NAME_SIZE 100
 #define OFFSET 40
-
-char PATH[MAX_FILE_NAME_SIZE];
 
 int startx = 0;
 int starty = 0;
@@ -26,87 +23,20 @@ char *file_list[MAX_FILE_NUMBER];
 int file_list_lenght = 0;
 
 intmax_t file_size_list[MAX_FILE_NUMBER] = {};
-DIR *d;
 struct dirent *dir;
 struct stat stat_buffer;
 int iter = 0;
 int highlight = 1;
 
-int main() {
-  WINDOW *menu_win;
-  int choice = 0;
-  int c;
+void dir_back(WINDOW *menu_win) {}
 
-  getcwd(PATH, MAX_FILE_NAME_SIZE);
-  initscr();
-  curs_set(0);
-  clear();
-  menu_win = newwin(LINES, COLS, starty, startx);
-  keypad(menu_win, true); // Arrow suuport
-  noecho();
-  // break(); /* Line buffering disabled. pass on everything */
-  get_dir_info();
-
-  while (1) {
-    print_menu(menu_win, highlight);
-    c = wgetch(menu_win);
-    switch (c) {
-    case 'q':
-      endwin();
-      return 0;
-      break;
-    case KEY_UP:
-      if (highlight == 1)
-        highlight = file_list_lenght;
-      else
-        --highlight;
-      break;
-    case 'k': // Same as KEY_UP
-      if (highlight == 1)
-        highlight = file_list_lenght;
-      else
-        --highlight;
-      break;
-    case KEY_DOWN:
-      if (highlight == file_list_lenght)
-        highlight = 1;
-      else
-        ++highlight;
-      break;
-    case 'j':
-      if (highlight == file_list_lenght)
-        highlight = 1;
-      else
-        ++highlight;
-      break;
-    case 'l':
-      endwin();
-      return 0;
-      break;
-    case 'h':
-      dir_back(menu_win);
-      break;
-    default:
-      refresh();
-      break;
-    }
-  }
-}
-
-void dir_back(WINDOW *menu_win) {
+void dir_forward() {
   // char *selection = {file_list[highlight - 1]};
   // wprintw(menu_win, "%s", selection);
   // wprintw(menu_win, "%s", PATH);
-  for (int i = 0; i < sizeof(PATH); i++) {
-    wprintw(menu_win, "%d", i);
-    wprintw(menu_win, "%c", PATH[i]);
-  }
-
-  // for (int i = (sizeof(PATH) - 1); i > -1; i--) {
-  // }
 }
 
-void get_dir_info() {
+void get_dir_info(WINDOW *menu_win) {
 
   // Free up string array
   for (int i = 0; i < file_list_lenght; i++) {
@@ -114,7 +44,9 @@ void get_dir_info() {
   }
 
   // Reset dir logic
-  d = opendir(PATH);
+  // wprintw(print_menu, PATH);
+  DIR *d;
+  d = opendir(".");
   iter = 0;
   file_list_lenght = 0;
   if (d) {
@@ -153,4 +85,66 @@ void print_menu(WINDOW *menu_win, int highlight) {
     ++y;
   }
   wrefresh(menu_win);
+}
+int main() {
+  WINDOW *menu_win;
+  int choice = 0;
+  int c;
+
+  initscr();
+  curs_set(0);
+  clear();
+  menu_win = newwin(LINES, COLS, starty, startx);
+  keypad(menu_win, true); // Arrow suuport
+  noecho();
+  // break(); /* Line buffering disabled. pass on everything */
+
+  while (1) {
+    wclear(menu_win);
+    get_dir_info(menu_win);
+    clear();
+    print_menu(menu_win, highlight);
+    c = wgetch(menu_win);
+    switch (c) {
+    case 'q':
+      endwin();
+      return 0;
+      break;
+    case KEY_UP:
+      if (highlight == 1)
+        highlight = file_list_lenght;
+      else
+        --highlight;
+      break;
+    case 'k': // Same as KEY_UP
+      if (highlight == 1)
+        highlight = file_list_lenght;
+      else
+        --highlight;
+      break;
+    case KEY_DOWN:
+      if (highlight == file_list_lenght)
+        highlight = 1;
+      else
+        ++highlight;
+      break;
+    case 'j':
+      if (highlight == file_list_lenght)
+        highlight = 1;
+      else
+        ++highlight;
+      break;
+    case 'l':
+      endwin();
+      return 0;
+      break;
+    case 'h':
+      chdir("..");
+      highlight = 1;
+      break;
+    default:
+      refresh();
+      break;
+    }
+  }
 }
